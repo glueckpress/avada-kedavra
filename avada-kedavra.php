447aa73f7d20e8cd5badfa6883e7e7388c9d29d9
Caspar Hübinger <caspar@glueckpress.com>
2014-11-11 11:07:50 +0100

    Formatting.

--- avada-kedavra.php
+++ avada-kedavra.php
@@ -4,7 +4,7 @@
  * Description: Disables ALL shortcodes at once.
  * Author:      Caspar Hübinger
  * Author URI:  http://glueckpress.com/
- * Plugin URI:  //github.com/glueckpress/avada-kedavra
+ * Plugin URI:  https://github.com/glueckpress/avada-kedavra
  * License:     GPLv2 or later
  * Version:     2014.11
  *
@@ -12,21 +12,21 @@
  */
 
 /**
-Copyright (C) 2014 Caspar Hübinger
+	Copyright (C) 2014 Caspar Hübinger
 
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
 /* Exit when accessed directly. */
